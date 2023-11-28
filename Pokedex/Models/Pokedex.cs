67d71d93a4bc1b325a114e3e082838deb5050774
jammyjoe <jamilmunayem@hotmail.com>
2023-11-28 17:58:13 +0000

    Add project files

---
+++ Pokedex/Models/Pokedex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokedex
+{
+    public partial class Pokedex
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Type1 { get; set; } = null!;
+        public string? Type2 { get; set; }
+    }
+}
---
+++ Pokedex/PokedexContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Pokedex
+{
+    public partial class PokedexContext : DbContext
+    {
+        public PokedexContext()
+        {
+        }
+
+        public PokedexContext(DbContextOptions<PokedexContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pokedex> Pokedices { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=Nika\\SQLEXPRESS;Initial Catalog=Pokedex;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pokedex>(entity =>
+            {
+                entity.ToTable("pokedex");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(256)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Type1)
+                    .HasMaxLength(55)
+                    .IsUnicode(false)
+                    .HasColumnName("type_1");
+
+                entity.Property(e => e.Type2)
+                    .HasMaxLength(55)
+                    .IsUnicode(false)
+                    .HasColumnName("type_2");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
