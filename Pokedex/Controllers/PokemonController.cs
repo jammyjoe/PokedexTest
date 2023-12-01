c368ffe2ec9b23fda8d6ce29b7232112e48d1066
jammyjoe <jamilmunayem@hotmail.com>
2023-12-01 11:05:56 +0000

    Add Controller

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+
+namespace Pokedex.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class PokemonController : ControllerBase
+	{
+		private readonly PokedexContext _context;
+
+		public PokemonController(PokedexContext context)
+        {
+            _context = context; 
+        }
+
+		[HttpGet]
+		public ActionResult<Pokemon> GetPokemon()
+		{
+			var pokedex = _context.Pokemons.OrderBy(p => p.Id).ToList();
+
+			if (!ModelState.IsValid)
+				return NoContent();
+
+			return Ok(pokedex);
+		}
+
+
+    }
+}
