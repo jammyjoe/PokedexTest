3867430131146505677c528e039010e6c9bc8a4a
jammyjoe <jamilmunayem@hotmail.com>
2023-12-02 14:28:47 +0000

    Add HTTP Methods (Post, Put, Delete) for PokemonController

--- Pokedex/Controllers/PokemonController.cs
+++ Pokedex/Controllers/PokemonController.cs
@@ -18,25 +18,76 @@ namespace Pokedex.Controllers
 		[HttpGet]
 		public ActionResult<Pokemon> GetPokemons()
 		{
-			var pokedex = _context.Pokemons.OrderBy(p => p.Id).ToList();
+			var pokemons = _context.Pokemons.OrderBy(p => p.Id).ToList();
 
 			if (!ModelState.IsValid)
 				return NoContent();
 
-			return Ok(pokedex);
+			return Ok(pokemons);
 		}
 
 		[HttpGet("{id}")]
 		public ActionResult<Pokemon> GetPokemon(int id)
 		{
-			var pokedex = _context.Pokemons.FirstOrDefault(p => p.Id == id);
+			var pokemon = _context.Pokemons.FirstOrDefault(p => p.Id == id);
 
 			if (!ModelState.IsValid)
 				return NoContent();
 
-			return Ok(pokedex);
+			return Ok(pokemon);
+		}
+
+		[HttpPost("{id}")]
+		public ActionResult<Pokemon> CreatePokemon(int id,
+			[FromBody] Pokemon pokemonCreate)
+		{
+			if (pokemonCreate == null)
+				return BadRequest("This Id is invalid");
+
+			_context.Add(pokemonCreate);
+
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			_context.SaveChanges();
+
+			return Ok(pokemonCreate);
 		}
 
 
+		[HttpPut("{id}")]
+		public ActionResult<Pokemon> UpdatePokemon(int id,
+	[FromBody] Pokemon pokemonCreate)
+		{
+			if (pokemonCreate == null)
+				return BadRequest("This Id is invalid");
+
+			_context.Update(pokemonCreate);
+
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			_context.SaveChanges();
+
+			return Ok(pokemonCreate);
+		}
+
+
+		[HttpDelete("{id}")]
+		public ActionResult<Pokemon> DeletePokemon(int id)
+		{
+			var pokemonToDelete = _context.Pokemons.Any(p => p.Id == id);
+			if (pokemonToDelete == null)
+				return NotFound("Sorry, but this pokemon doesn't exist");
+
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			_context.Remove(true);
+
+			_context.SaveChanges();
+
+			return Ok(_context);
+		}
 	}
 }
