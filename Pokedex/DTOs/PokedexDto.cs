adc8655325e9c55a92a6712ad5a1745737462679
jammyjoe <jamilmunayem@hotmail.com>
2023-12-05 17:27:31 +0000

    Add DTO

---
+++ Pokedex/DTOs/PokedexDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokedex.DTOs
+{
+	public record PokedexDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string Type1 { get; set; } = null!;
+		public string? Type2 { get; set; }
+	}
+}
