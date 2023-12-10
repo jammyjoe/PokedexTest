08d3963b5997270d153ab9772729d8d0cd11a5ba
jammyjoe <jamilmunayem@hotmail.com>
2023-12-10 15:29:52 +0000

    Add repository async methods

--- Pokedex/Repository/PokemonRepository.cs
+++ Pokedex/Repository/PokemonRepository.cs
@@ -1,4 +1,5 @@
-﻿using Pokedex.Models;
+﻿using System.Data.Entity;
+using Pokedex.Models;
 using Pokedex.RepositoryInterface;
 
 namespace Pokedex.Repository
@@ -12,39 +13,39 @@ namespace Pokedex.Repository
 			_context = context;
 		}
 
-		public ICollection<Pokemon> GetPokemons()
+		public async Task<ICollection<Pokemon>> GetPokemons()
 		{
-			return _context.Pokemons.OrderBy(p => p.Id).ToList();
+			return await _context.Pokemons.OrderBy(p => p.Id).ToListAsync();
 		}
-		public Pokemon GetPokemon(int id)
+		public async Task<Pokemon> GetPokemon (int id)
 		{
-			return _context.Pokemons.FirstOrDefault(p => p.Id == id);
+			return await _context.Pokemons.FirstOrDefaultAsync(p => p.Id == id);
 		}
 
-		public bool CreatePokemon(Pokemon pokemon)
+		public async Task<bool> CreatePokemon(Pokemon pokemon)
 		{
 			_context.Add(pokemon);
-			return SavePokemon();
+			return await SavePokemon();
 		}
 
-		public bool DeletePokemon(Pokemon pokemon)
+		public async Task<bool> DeletePokemon(Pokemon pokemon)
 		{
 			_context.Remove(pokemon);
-			return SavePokemon();
+			return await SavePokemon();
 		}
 
-		public bool UpdatePokemon(Pokemon pokemon)
+		public async Task <bool> UpdatePokemon(Pokemon pokemon)
 		{
 			_context.Update(pokemon);
-			return SavePokemon();
+			return await SavePokemon();
 		}
 
-		public bool PokemonExists(int id)
+		public async Task<bool> PokemonExists(int id)
 		{
-			return _context.Pokemons.Any(p => p.Id == id);
+			return await _context.Pokemons.AnyAsync(p => p.Id == id);
 		}
 
-		public bool SavePokemon()
+		public async Task<bool> SavePokemon()
 		{
 			var saved = _context.SaveChanges();
 			return saved > 0 ? true : false;
