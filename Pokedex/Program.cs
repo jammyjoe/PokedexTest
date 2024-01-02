3dac048eff8716d5546b5cf99d7dfd5f6fdc10be
Jamil Munayem <AJamil@Engineering.ukho.gov.uk>
2024-01-02 09:56:14 +0000

    Remove swagger/swashbuckle

--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -13,6 +13,7 @@ namespace Pokedex
 
 			// Add services to the container.
 			builder.Services.AddControllers();
+			builder.Services.AddRazorPages();
 			builder.Services.AddResponseCaching(x => x.MaximumBodySize = 1024);
 			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 			builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
@@ -20,23 +21,29 @@ namespace Pokedex
 			builder.Services.AddDbContext<PokedexContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+			//builder.Services.AddEndpointsApiExplorer();
+			//builder.Services.AddSwaggerGen();
 
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+			//if (app.Environment.IsDevelopment())
+			//{
+			//	app.UseSwagger();
+			//	app.UseSwaggerUI();
+			//}
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
 
 			app.UseHttpsRedirection();
 
 			app.UseAuthorization();
 
 			app.UseResponseCaching();
+
+            app.MapRazorPages();
 
 			app.MapControllers();
 
--- Pokedex/obj/Debug/net6.0/PokedexAPI.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net6.0/PokedexAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0df98875b090385eafd0a17b1554955f69b6ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
