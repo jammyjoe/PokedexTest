845110879c871c2b595d96c0fac8683120c3a461
MunayemJ <MunayemJ@HXDTENGMS122>
2024-01-03 08:40:56 +0000

    Add Tailwindcss to pages

--- PokedexApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokedexApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
