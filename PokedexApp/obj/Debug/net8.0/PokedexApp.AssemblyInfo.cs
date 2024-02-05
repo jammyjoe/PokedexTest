08f64e835273563a6d16bd2a5d1f8c72026ffb44
MunayemJ <MunayemJ@HXDTENGMS122>
2024-02-05 14:26:21 +0000

    Fix

--- PokedexApp/obj/Debug/net8.0/PokedexApp.AssemblyInfo.cs
+++ PokedexApp/obj/Debug/net8.0/PokedexApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e3ac4fb88282bbadffd6d35b24057888e5d639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
